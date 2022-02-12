aae0e06c103e7bc551b56aa7fffe825b5a8b2ccf
jenboc <47900748+jenboc@users.noreply.github.com>
2022-02-12 15:05:17 +0000

    Menu bar (no functionality)

--- jenboc_paint.Designer.cs
+++ jenboc_paint.Designer.cs
@@ -30,22 +30,30 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(jenboc_paint));
             this.graphicsPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.thicknessTrackBar = new System.Windows.Forms.TrackBar();
             this.panel2 = new System.Windows.Forms.Panel();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.thicknessTrackBar = new System.Windows.Forms.TrackBar();
-            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changePenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.graphicsPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.thicknessTrackBar)).BeginInit();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.thicknessTrackBar)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // graphicsPanel
@@ -54,6 +62,7 @@
             this.graphicsPanel.Controls.Add(this.label1);
             this.graphicsPanel.Controls.Add(this.thicknessTrackBar);
             this.graphicsPanel.Controls.Add(this.panel2);
+            this.graphicsPanel.Controls.Add(this.menuStrip1);
             this.graphicsPanel.Cursor = System.Windows.Forms.Cursors.Cross;
             this.graphicsPanel.Location = new System.Drawing.Point(0, 0);
             this.graphicsPanel.Name = "graphicsPanel";
@@ -63,6 +72,32 @@
             this.graphicsPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.graphicsPanel_MouseMove);
             this.graphicsPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.graphicsPanel_MouseUp);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(931, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Pen Thickness";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // thicknessTrackBar
+            // 
+            this.thicknessTrackBar.AutoSize = false;
+            this.thicknessTrackBar.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.thicknessTrackBar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.thicknessTrackBar.Location = new System.Drawing.Point(919, 74);
+            this.thicknessTrackBar.Minimum = 3;
+            this.thicknessTrackBar.Name = "thicknessTrackBar";
+            this.thicknessTrackBar.Size = new System.Drawing.Size(280, 57);
+            this.thicknessTrackBar.TabIndex = 2;
+            this.thicknessTrackBar.Value = 3;
+            this.thicknessTrackBar.ValueChanged += new System.EventHandler(this.changePenThickness);
+            // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
@@ -71,7 +106,7 @@
             this.panel2.Controls.Add(this.pictureBox3);
             this.panel2.Controls.Add(this.pictureBox2);
             this.panel2.Controls.Add(this.pictureBox1);
-            this.panel2.Location = new System.Drawing.Point(916, 12);
+            this.panel2.Location = new System.Drawing.Point(919, 12);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(283, 56);
             this.panel2.TabIndex = 1;
@@ -131,31 +166,71 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.changePenColor);
             // 
-            // thicknessTrackBar
+            // menuStrip1
             // 
-            this.thicknessTrackBar.AutoSize = false;
-            this.thicknessTrackBar.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.thicknessTrackBar.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.thicknessTrackBar.Location = new System.Drawing.Point(919, 74);
-            this.thicknessTrackBar.Minimum = 3;
-            this.thicknessTrackBar.Name = "thicknessTrackBar";
-            this.thicknessTrackBar.Size = new System.Drawing.Size(280, 57);
-            this.thicknessTrackBar.TabIndex = 2;
-            this.thicknessTrackBar.Value = 3;
-            this.thicknessTrackBar.ValueChanged += new System.EventHandler(this.changePenThickness);
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.settingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1215, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
             // 
-            // label1
+            // fileToolStripMenuItem
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label1.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(931, 108);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(151, 16);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Pen Thickness";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.fileToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.changePenToolStripMenuItem});
+            this.settingsToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.saveToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.saveAsToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.closeToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            // 
+            // changePenToolStripMenuItem
+            // 
+            this.changePenToolStripMenuItem.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.changePenToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.changePenToolStripMenuItem.Name = "changePenToolStripMenuItem";
+            this.changePenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.changePenToolStripMenuItem.Text = "Change Pen";
             // 
             // jenboc_paint
             // 
@@ -164,17 +239,21 @@
             this.ClientSize = new System.Drawing.Size(1211, 651);
             this.Controls.Add(this.graphicsPanel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(1227, 690);
+            this.MinimumSize = new System.Drawing.Size(1227, 690);
             this.Name = "jenboc_paint";
             this.Text = "Jenboc Paint";
             this.graphicsPanel.ResumeLayout(false);
             this.graphicsPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.thicknessTrackBar)).EndInit();
             this.panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.thicknessTrackBar)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -190,6 +269,13 @@
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.TrackBar thicknessTrackBar;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem changePenToolStripMenuItem;
     }
 }
 
