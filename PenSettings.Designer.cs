202d2e8616cf0999b1b0601bb0377d139fcd1253
jenboc <47900748+jenboc@users.noreply.github.com>
2022-02-22 20:44:47 +0000

    Changed Pen Settings width

--- PenSettings.Designer.cs
+++ PenSettings.Designer.cs
@@ -250,7 +250,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(623, 172);
+            this.ClientSize = new System.Drawing.Size(493, 172);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.thicknessTrackBar);
